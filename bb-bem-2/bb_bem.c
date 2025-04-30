b235988d0b5a04693c63f880d311ccd97b7a0ec7
sashi0034 <nishida.satoshi.23e@st.kyoto-u.ac.jp>
2025-04-30 12:54:45 +0900

    Add current WIP files

---
+++ bb-bem-2/bb_bem.c
@@ -0,0 +1,268 @@
+﻿#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+#include "bb_bem.h"
+
+#include "bicgstab_naive.h"
+#include "bicgstab_cuda.h"
+
+#if !defined(BB_NO_MAIN)
+int main() {
+    bb_result_t result;
+    bb_bem("input.txt", BB_COMPUTE_CUDA, &result); // CUDA で実行
+
+    // ----------------------------------------------- fp
+    FILE* fp = fopen("out2.data", "w");
+    for (int i = 0; i < result.dim; i++) {
+        fprintf(fp, "%20.14e \n", result.sol[i]);
+    }
+
+    fclose(fp);
+    // -----------------------------------------------
+
+    release_bb_result(&result);
+
+    return 0;
+}
+#endif
+
+// -----------------------------------------------
+
+static void** allocate_matrix(size_t rows, size_t cols, size_t elem_size) {
+    void** array = (void**)malloc(sizeof(void*) * rows);
+    if (!array) return NULL;
+
+    array[0] = malloc(rows * cols * elem_size);
+    if (!array[0]) {
+        free(array);
+        return NULL;
+    }
+
+    for (size_t i = 1; i < rows; i++) {
+        array[i] = (uint8_t*)array[0] + i * cols * elem_size;
+    }
+
+    return array;
+}
+
+static void release_matrix(void** matrix) {
+    if (matrix && matrix[0]) free(matrix[0]);
+    if (matrix) free(matrix);
+}
+
+static void transpose_double_matrix(size_t rows, size_t cols, double** mat, double** mat_T) {
+    for (size_t row = 0; row < rows; ++row) {
+        for (size_t col = 0; col < cols; ++col) {
+            mat_T[col][row] = mat[row][col];
+        }
+    }
+}
+
+// -----------------------------------------------
+
+#define NUMBER_ELEMENT_DOF  1;
+
+#define TOR 1e-8 // Tolerance for convergence
+
+#define MAX_STEPS 1000 // Maximum number of iterations
+
+static bb_status_t read_input_from_file(const char* filename, bb_input_t* input) {
+    FILE* fp = fopen(filename, "r");
+    if (!fp) {
+        printf("Error: Cannot open file %s\n", filename);
+        return BB_ERR_FILE_OPEN;
+    }
+
+    int success = 0;
+
+    // Read the number of nodes from an input data file: nond  
+    if (fscanf(fp, "%d", &input->nond) != 1) goto fail;
+
+    // Allocation for the array for the coordinates of the nodes 
+    input->np = (vector3_t*)malloc(sizeof(vector3_t) * input->nond);
+    if (!input->np) goto bad_alloc;
+
+    // Read the coordinates of the nodes from an input data file: np  
+    for (int i = 0; i < input->nond; i++) {
+        if (fscanf(fp, "%lf %lf %lf", &(input->np[i].x), &(input->np[i].y), &(input->np[i].z)) != 3) {
+            goto fail;
+        }
+    }
+
+    // Read the number of faces from an input data file: nofc  
+    if (fscanf(fp, "%d", &input->nofc) != 1) goto fail;
+
+    // Read the number of nodes on each face from an input data file: nond_on_face  
+    if (fscanf(fp, "%d", &input->nond_on_face) != 1) goto fail;
+
+    // Read the number of integer parameters set on each face from an input data file: nint_para_fc  
+    if (fscanf(fp, "%d", &input->nint_para_fc) != 1) goto fail;
+
+    // Read the number of real(double precision) parameters set on each face from an input data file: ndble_para_fc  
+    if (fscanf(fp, "%d", &input->ndble_para_fc) != 1) goto fail;
+
+    // Read the number of parameters set on each face from an input data file: para_batch
+    if (fscanf(fp, "%d", &input->para_batch) != 1) goto fail;
+
+    printf("Number of nodes=%d Number of faces=%d\n", input->nond, input->nofc);
+
+    // -----------------------------------------------
+
+    // face2node
+    input->face2node = (int**)allocate_matrix(input->nofc, input->nond_on_face, sizeof(int));
+    if (!input->face2node) goto bad_alloc;
+
+    for (int i = 0; i < input->nofc; i++) {
+        for (int j = 0; j < input->nond_on_face; j++) {
+            if (fscanf(fp, "%d", &input->face2node[i][j]) != 1) goto fail;
+        }
+    }
+
+    // int_para_fc
+    if (input->nint_para_fc > 0) {
+        input->int_para_fc = (int***)malloc(sizeof(int**) * input->para_batch);
+        for (int n = 0; n < input->para_batch; n++) {
+            input->int_para_fc[n] = (int**)allocate_matrix(input->nofc, input->nint_para_fc, sizeof(int));
+            if (!input->int_para_fc[n]) goto bad_alloc;
+
+            for (int i = 0; i < input->nofc; i++) {
+                for (int j = 0; j < input->nint_para_fc; j++) {
+                    if (fscanf(fp, "%d", &input->int_para_fc[n][i][j]) != 1) goto fail;
+                }
+            }
+        }
+    }
+
+    // dble_para_fc
+    if (input->ndble_para_fc > 0) {
+        input->dble_para_fc = (double***)malloc(sizeof(double**) * input->para_batch);
+        for (int n = 0; n < input->para_batch; n++) {
+            input->dble_para_fc[n] = (double**)allocate_matrix(input->nofc, input->ndble_para_fc, sizeof(double));
+            if (!input->dble_para_fc[n]) goto bad_alloc;
+
+            for (int i = 0; i < input->nofc; i++) {
+                for (int j = 0; j < input->ndble_para_fc; j++) {
+                    if (fscanf(fp, "%lf", &input->dble_para_fc[n][i][j]) != 1) goto fail;
+                }
+            }
+        }
+    }
+
+    success = 1;
+
+fail:
+    if (!success) {
+        fclose(fp);
+        printf("Error: Invalid file format %s\n", filename);
+        return BB_ERR_FILE_FORMAT;
+    }
+
+bad_alloc:
+    if (!input->np) {
+        fclose(fp);
+        printf("Error: Out of memory while reading %s\n", filename);
+        return BB_ERR_FILE_OPEN;
+    }
+
+    fclose(fp);
+    return BB_OK;
+}
+
+bb_status_t bb_bem(const char* filename, bb_compute_t /* in */ compute, bb_result_t* result) {
+    bb_input_t* input = &result->input;
+    *input = (bb_input_t){0}; // Initialize input structure
+
+    const bb_status_t input_status = read_input_from_file(filename, input);
+    if (input_status != BB_OK) {
+        return input_status;
+    }
+
+    // -----------------------------------------------
+
+    result->dim = input->nofc * NUMBER_ELEMENT_DOF;
+
+    double** A = (double**)allocate_matrix(result->dim, result->dim, sizeof(double));
+    if (!A) return BB_ERR_MEMORY_ALLOC;
+
+    double** rhs = (double**)allocate_matrix(result->dim, input->para_batch, sizeof(double));
+    if (!rhs) return BB_ERR_MEMORY_ALLOC;
+
+    result->sol = (double**)allocate_matrix(result->dim, input->para_batch, sizeof(double));
+    if (!result->sol) return BB_ERR_MEMORY_ALLOC;
+
+    for (int i = 0; i < result->dim; i++) {
+        for (int n = 0; n < input->para_batch; n++) {
+            result->sol[i][n] = 0.0;
+        }
+    }
+
+    // User Specified Function 
+    // element_integral(coordinate np, double **a, ); 
+
+    for (int i = 0; i < result->dim; i++) {
+        for (int j = 0; j < result->dim; j++) {
+            A[i][j] = element_ij_(&i, &j, &input->nond, &input->nofc, &input->np[0], &input->face2node[0][0]);
+        }
+    }
+
+    for (int i = 0; i < result->dim; i++) {
+        for (int n = 0; n < input->para_batch; n++) {
+            rhs[i][n] = input->dble_para_fc[n][i][0]; // TODO
+        }
+    }
+
+    printf("Linear system was generated.\n");
+
+    if (compute == BB_COMPUTE_NAIVE) {
+        bicgstab_naive(input->para_batch, result->dim, A, rhs, result->sol, TOR, MAX_STEPS);
+    }
+    else {
+        bicgstab_cuda(input->para_batch, result->dim, A, rhs, result->sol, TOR, MAX_STEPS);
+    }
+
+    printf("OK\n");
+
+    release_matrix(A);
+
+    release_matrix(rhs);
+
+    return BB_OK;
+}
+
+void release_bb_result(bb_result_t* result) {
+    if (!result) return;
+
+    bb_input_t* input = &result->input;
+
+    free(input->np);
+
+    if (input->face2node) {
+        release_matrix(input->face2node);
+    }
+
+    if (input->int_para_fc) {
+        for (int n = 0; n < input->para_batch; n++) { release_matrix(input->int_para_fc[n]); }
+
+        free(input->int_para_fc);
+    }
+
+    if (input->dble_para_fc) {
+        for (int n = 0; n < input->para_batch; n++) { release_matrix(input->dble_para_fc[n]); }
+
+        free(input->dble_para_fc);
+    }
+
+    release_matrix(result->sol);
+
+    input->np = NULL;
+    input->face2node = NULL;
+    input->int_para_fc = NULL;
+    input->dble_para_fc = NULL;
+    result->sol = NULL;
+}
---
+++ bb-bem-2/bb_bem.h
@@ -0,0 +1,113 @@
+﻿#ifndef BB_BEM_H
+#define BB_BEM_H
+
+// ユーザー側で定義される関数のアトリビュート
+#define BB_USER_FUNC extern
+
+#define BB_API
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct vector3_t {
+    double x, y, z;
+} vector3_t;
+
+/// @brief ユーザー側で定義される、計算対象要素における i, j 節点間の物理量（例: 積分値）を返す関数
+///
+/// この関数は、境界要素法による 3 次元問題の離散化モデルに基づき、
+/// 指定された要素内の2節点（i, j）に関連する物理量を計算します。
+///
+/// @param[in] i 節点 i のインデックス（0-origin）
+/// @param[in] j 節点 j のインデックス（0-origin）
+/// @param[in] nond 全体の節点数
+/// @param[in] nofc 全体の要素数
+/// @param[in] np 節点の3次元座標値配列（サイズ: [3][*nond]）
+/// @param[in] face2node 各要素を構成する節点番号の配列（サイズ: [*nond_on_face][*nofc]）
+///
+/// @return 要素内の i, j 節点間の計算結果（例: 積分値）
+///
+/// @note 各要素は多角形（三角形など）であり、その頂点は節点として表される。
+/// 節点座標 `np` および構成節点番号 `face2node` を用いて各要素の幾何情報を参照する。
+/// 
+/// @remark Fortran の場合は 'real(8) function element_ij(i, j, nond, nofc, np, face2node)' として定義してください。
+BB_USER_FUNC double element_ij_(
+    int* i /* in */,
+    int* j /* in */,
+    int* nond /* in */,
+    int* nofc /* in */,
+    vector3_t* np /* in */,
+    int* face2node /* in */
+);
+
+// -----------------------------------------------
+
+/// @brief ファイルから読み込まれる入力データ構造体
+typedef struct bb_input_t {
+    /// @brief 節点数 (Number of nodes)
+    int nond;
+
+    /// @brief 要素数 (Number of faces/elements) 
+    int nofc;
+
+    /// @brief 各要素を構成する節点数 (Nodes per face) 
+    int nond_on_face;
+
+    /// @brief 各要素上で定義される int 型パラメータ数 
+    int nint_para_fc;
+
+    /// @brief 各要素上で定義される double 型パラメータ数 
+    int ndble_para_fc;
+
+    /// @brief 各要素におけるパラメータのバッチ数
+    int para_batch;
+
+    /// @brief 節点座標 (サイズ: nond) 
+    vector3_t* np;
+
+    /// @brief 各要素を構成する節点番号 (サイズ: nofc * nond_on_face)
+    int** face2node;
+
+    /// @brief 各要素の int パラメータ (サイズ: para_batch * nofc * nint_para_fc)
+    int*** int_para_fc;
+
+    /// @brief 各要素の double パラメータ (サイズ: para_batch * nofc * ndble_para_fc)
+    double*** dble_para_fc;
+} bb_input_t;
+
+/// @brief BEM の計算結果を格納する構造体
+typedef struct bb_result_t {
+    /// @brief 入力データ
+    bb_input_t input;
+
+    /// @brief 解ベクトルのサイズ (サイズ: nofc * 1)
+    int dim;
+
+    /// @brief 解ベクトル (サイズ: dim * batch)
+    double** sol;
+} bb_result_t;
+
+typedef enum {
+    BB_OK = 0, // 成功
+    BB_ERR_FILE_OPEN, // ファイルが開けなかった
+    BB_ERR_FILE_FORMAT, // ファイルフォーマット不正
+    BB_ERR_MEMORY_ALLOC, // メモリ確保失敗
+    BB_ERR_UNKNOWN // その他不明なエラー
+} bb_status_t;
+
+typedef enum {
+    BB_COMPUTE_NAIVE,
+    BB_COMPUTE_CUDA,
+} bb_compute_t;
+
+/// @brief BEM の計算を実行する関数
+BB_API bb_status_t bb_bem(const char* filename /* in */, bb_compute_t /* in */ compute, bb_result_t* result /* out */);
+
+BB_API void release_bb_result(bb_result_t* result /* in */);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BB_BEM_H
